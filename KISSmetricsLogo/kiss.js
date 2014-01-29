abd46a80416fefe39fe9e287ac4f96813c624a0f
Ben Angel <benjamin.b2@gmail.com>
2014-01-28 16:51:15 -0800

    css, html, and js for KISSmetrics logo

---
+++ KISSmetricsLogo/kiss.js
@@ -0,0 +1,9 @@
+setTimeout(function(){
+  $('.one').children().addClass('pullUp');
+}, 500);
+setTimeout(function(){
+  $('.two').children().addClass('pullUp');
+}, 3400);
+setTimeout(function(){
+  $('.three').children().addClass('display');
+}, 5500);
